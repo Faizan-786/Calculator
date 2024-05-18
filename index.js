431d12b0075c4ed62ad66769b9f8cf2e8fdf84db
Faizan-786 <faizanaliakbar125@gmail.com>
2024-05-18 02:09:42 -0700

    updates

---
+++ index.js
@@ -0,0 +1,43 @@
+import inquirer from "inquirer";
+async function calculator() {
+    let response = await inquirer.prompt([
+        {
+            name: "firstNumber",
+            message: "Enter First Number",
+            type: "number",
+        },
+        {
+            name: "secondNumber",
+            message: "Enter Second Number",
+            type: "number",
+        },
+        {
+            name: "operator",
+            message: "Select Operator",
+            //   type: "list",
+            type: "checkbox",
+            choices: ["+", "-", "/", "*"],
+        },
+    ]);
+    const { firstNumber, secondNumber, operator } = response;
+    if (operator === "+") {
+        let addition = firstNumber + secondNumber;
+        console.log(`Sum of these numbers is ${addition}`);
+    }
+    else if (operator === "-") {
+        let substraction = firstNumber - secondNumber;
+        console.log(`Substraction of these numbers is ${substraction}`);
+    }
+    else if (operator === "/") {
+        let division = firstNumber / secondNumber;
+        console.log(`Division of these numbers is ${division}`);
+    }
+    else if (operator === "*") {
+        let multiplication = firstNumber * secondNumber;
+        console.log(`Multiplication of these numbers is ${multiplication}`);
+    }
+    else {
+        console.log("Please Select the right Operator");
+    }
+}
+calculator();
